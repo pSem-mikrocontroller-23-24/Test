684973cae4342fa88000a49151ed2158a254635b
frau-eckstein <47317081+frau-eckstein@users.noreply.github.com>
2024-04-18 14:12:36 +0200

    Update rgb-color-detection.py

--- rgb-color-detection.py
+++ rgb-color-detection.py
@@ -42,7 +42,7 @@ with dai.Device(pipeline) as device:
         inRgb = qRgb.get()  # blocking call, will wait until a new data has arrived
 
         # Retrieve 'bgr' (opencv format) frame
-        # cv2.imshow("rgb", inRgb.getCvFrame())
+        cv2.imshow("rgb", inRgb.getCvFrame())
 
     # Convert the imageFrame in  
     # BGR(RGB color space) to  
