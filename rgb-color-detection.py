7b2cf376e0983a66053352861244cd8294d9e2b7
frau-eckstein <47317081+frau-eckstein@users.noreply.github.com>
2024-04-25 14:23:26 +0200

    Update rgb-color-detection.py

--- rgb-color-detection.py
+++ rgb-color-detection.py
@@ -1,110 +1,45 @@
-#!/usr/bin/env python3
-
-# vgl. https://github.com/luxonis/depthai-python/blob/main/examples/ColorCamera/rgb_preview.py
-
-import numpy as np 
-import cv2
 import depthai as dai
+import numpy as np
+import cv2
 
-# Create pipeline
+# Erstelle eine Pipeline
 pipeline = dai.Pipeline()
 
-# Define source and output
-camRgb = pipeline.create(dai.node.ColorCamera)
-xoutRgb = pipeline.create(dai.node.XLinkOut)
+# Konfiguriere Kameraeinstellungen
+cam_rgb = pipeline.createColorCamera()
+cam_rgb.setPreviewSize(300, 300)
+cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+cam_rgb.setInterleaved(False)
+cam_rgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
 
-xoutRgb.setStreamName("rgb")
+# Erstelle Output Streams
+xout_rgb = pipeline.createXLinkOut()
+xout_rgb.setStreamName("rgb")
+cam_rgb.preview.link(xout_rgb.input)
 
-# Properties
-camRgb.setPreviewSize(500, 500)
-camRgb.setInterleaved(False)
-camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
-
-# Linking
-camRgb.preview.link(xoutRgb.input)
-
-# Connect to device and start pipeline
+# Starte die Pipeline
 with dai.Device(pipeline) as device:
-
-    print('Connected cameras:', device.getConnectedCameraFeatures())
-    # Print out usb speed
-    print('Usb speed:', device.getUsbSpeed().name)
-    # Bootloader version
-    if device.getBootloaderVersion() is not None:
-        print('Bootloader version:', device.getBootloaderVersion())
-    # Device name
-    print('Device name:', device.getDeviceName(), ' Product name:', device.getProductName())
-
-    # Output queue will be used to get the rgb frames from the output defined above
-    qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+    # Starte den Preview-Stream
+    q_rgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
 
     while True:
-        inRgb = qRgb.get()  # blocking call, will wait until a new data has arrived
+        # Warte auf ein neues Bild
+        in_rgb = q_rgb.get()
+        # Konvertiere Bild zu numpy array
+        frame = in_rgb.getCvFrame()
+        
+        # Finde den vorherrschenden RGB-Wert im Bild
+        dominant_color = np.mean(frame, axis=(0, 1))
 
-        # Retrieve 'bgr' (opencv format) frame
-        cv2.imshow("rgb", inRgb.getCvFrame())
+        # Zeige den vorherrschenden RGB-Wert im Bild an
+        cv2.putText(frame, f"RGB: {dominant_color}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        
+        # Zeige das Bild mit markiertem RGB-Wert an
+        cv2.imshow("RGB Image", frame)
 
-    # Convert the imageFrame in  
-    # BGR(RGB color space) to  
-    # HSV(hue-saturation-value) 
-    # color space 
-    hsvFrame = cv2.cvtColor(inRgb, cv2.COLOR_BGR2HSV) 
-  
-    # Set range for red color and  
-    # define mask 
-    red_lower = np.array([136, 87, 111], np.uint8) 
-    red_upper = np.array([180, 255, 255], np.uint8) 
-    red_mask = cv2.inRange(hsvFrame, red_lower, red_upper) 
-  
-    # Set range for green color and  
-    # define mask 
-    green_lower = np.array([25, 52, 72], np.uint8) 
-    green_upper = np.array([102, 255, 255], np.uint8) 
-    green_mask = cv2.inRange(hsvFrame, green_lower, green_upper) 
-  
-    # Set range for blue color and 
-    # define mask 
-    blue_lower = np.array([94, 80, 2], np.uint8) 
-    blue_upper = np.array([120, 255, 255], np.uint8) 
-    blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper) 
-      
-    # Morphological Transform, Dilation 
-    # for each color and bitwise_and operator 
-    # between imageFrame and mask determines 
-    # to detect only that particular color 
-    kernel = np.ones((5, 5), "uint8") 
-      
-    # For red color 
-    red_mask = cv2.dilate(red_mask, kernel) 
-    res_red = cv2.bitwise_and(imageFrame, imageFrame,  
-                              mask = red_mask) 
-      
-    # For green color 
-    green_mask = cv2.dilate(green_mask, kernel) 
-    res_green = cv2.bitwise_and(imageFrame, imageFrame, 
-                                mask = green_mask) 
-      
-    # For blue color 
-    blue_mask = cv2.dilate(blue_mask, kernel) 
-    res_blue = cv2.bitwise_and(imageFrame, imageFrame, 
-                               mask = blue_mask) 
-   
-    # Creating contour to track red color 
-    contours, hierarchy = cv2.findContours(red_mask, 
-                                           cv2.RETR_TREE, 
-                                           cv2.CHAIN_APPROX_SIMPLE)
-    for pic, contour in enumerate(contours): 
-        area = cv2.contourArea(contour) 
-        if(area > 300): 
-            x, y, w, h = cv2.boundingRect(contour) 
-            imageFrame = cv2.rectangle(imageFrame, (x, y),  
-                                       (x + w, y + h),  
-                                       (0, 0, 255), 2) 
-              
-            cv2.putText(imageFrame, "Red Colour", (x, y), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
-                        (0, 0, 255))
-  
-
+        # Warte auf das Schließen des Fensters
         if cv2.waitKey(1) == ord('q'):
             break
+
+    # Beende die OpenCV-Anzeige
+    cv2.destroyAllWindows()
